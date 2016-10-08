cbcaba5c8a494b1eac1e55ad94622c389c37c16d
srini <srinivasmolleti@gmail.com>
2016-10-09 01:32:34 +0800

    tp 1004: Build Error fixed.

--- LandInformationSystem/Form1.Designer.cs
+++ LandInformationSystem/Form1.Designer.cs
@@ -45,9 +45,9 @@
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(96, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(339, 25);
+            this.label1.Size = new System.Drawing.Size(335, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to LandGate Application";
+            this.label1.Text = "Welcome to Landgate Application";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
